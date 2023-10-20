b034dfaa932d6ad9f83969ad92af8fce2c154ba2
Masataka ISHII <masataka.ishii@soat.fr>
2023-10-20 14:05:34 +0200

    refactoring methods WIP
    few tests not passed

--- Java/src/main/java/algorithm/Finder.java
+++ Java/src/main/java/algorithm/Finder.java
@@ -1,54 +1,55 @@
 package algorithm;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Finder {
-    private final List<Person> persons;
+  private final List<Person> persons;
 
-    public Finder(List<Person> persons) {
-        this.persons = persons;
+  public Finder(List<Person> persons) {
+    this.persons = persons;
+  }
+
+  public Couple find(ExtremityAge extremityAge) {
+    List<Couple> couples = getCouples();
+
+    return findByExtremityAge(extremityAge, couples).orElse(new Couple());
+  }
+
+  private List<Couple> getCouples() {
+    List<Couple> couples = new ArrayList<>();
+
+    for (int i = 0; i < persons.size() - 1; i++) {
+      for (int j = i + 1; j < persons.size(); j++) {
+        Couple couple = new Couple();
+        if (persons.get(i).birthDate.getTime() < persons.get(j).birthDate.getTime()) {
+          couple.oldestPerson = persons.get(i);
+          couple.youngestPerson = persons.get(j);
+        } else {
+          couple.oldestPerson = persons.get(j);
+          couple.youngestPerson = persons.get(i);
+        }
+        couple.ageDifference = couple.youngestPerson.birthDate.getTime() - couple.oldestPerson.birthDate.getTime();
+        couples.add(couple);
+      }
+    }
+    return couples;
+  }
+
+  private static Optional<Couple> findByExtremityAge(ExtremityAge extremityAge, List<Couple> couples) {
+    switch (extremityAge) {
+      case ClosestAge -> {
+        return couples.stream()
+            .sorted(Comparator.comparing((c) -> c.ageDifference))
+            .findFirst();
+      }
+      case FarthestAges -> {
+        return couples.stream()
+            .sorted( Comparator.comparing((c) -> c.ageDifference))
+            .sorted(Collections.reverseOrder())
+            .findFirst();
+      }
     }
 
-    public Couple find(ExtremityAge extremityAge) {
-        List<Couple> couples = new ArrayList<>();
-
-        for (int i = 0; i < persons.size() - 1; i++) {
-            for (int j = i + 1; j < persons.size(); j++) {
-                Couple couple = new Couple();
-                if (persons.get(i).birthDate.getTime() < persons.get(j).birthDate.getTime()) {
-                    couple.oldestPerson = persons.get(i);
-                    couple.youngestPerson = persons.get(j);
-                } else {
-                    couple.oldestPerson = persons.get(j);
-                    couple.youngestPerson = persons.get(i);
-                }
-                couple.ageDifference = couple.youngestPerson.birthDate.getTime() - couple.oldestPerson.birthDate.getTime();
-                couples.add(couple);
-            }
-        }
-
-        if (couples.size() < 1) {
-            return new Couple();
-        }
-
-        Couple couple = couples.get(0);
-        for (Couple a : couples) {
-            switch (extremityAge) {
-            case ClosestAge:
-                if (a.ageDifference < couple.ageDifference) {
-                    couple = a;
-                }
-                break;
-
-            case FarthestAges:
-                if (a.ageDifference > couple.ageDifference) {
-                    couple = a;
-                }
-                break;
-            }
-        }
-
-        return couple;
-    }
+    return Optional.empty();
+  }
 }
